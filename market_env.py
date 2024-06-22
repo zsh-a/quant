d12764f29b7582ddff8ce7f37022c0063f34f0e7
zsh <2494726068@qq.com>
2024-06-23 01:18:46 +0800

    refactor for three policy

--- market_env.py
+++ market_env.py
@@ -4,8 +4,8 @@ import numpy as np
 from data_source import DataSource
 import matplotlib.pyplot as plt
 
+from utils import log2percent
 
-from utils import *
 INF = 1e9
 class MarketEnv(gym.Env):
     def __init__(self,num_step,code='510880',start_date='20100531',end_date='20110901',work_dir='',initial_capital=100000,max_stake=10000) -> None:
@@ -40,27 +40,62 @@ class MarketEnv(gym.Env):
         self.market_returns = np.zeros(self.num_step + 1)
 
 
+        self.buy_sell_points = []
+
+        self.last_obs = None
+
+        self.ordering = 0
+
+        self.waiting_buy = 0
+
     def step(self, action: Any) -> Tuple[Any | SupportsFloat | bool | dict[str, Any]]:
         # assert self.action_space.contains(action)
         obs,done,ori_obs = self.data_source.step()
+        # if self.ordering != 0 and self.ordering * action >= 0:
+        #     action = self.ordering
+        # else:
+        #     self.ordering = 0
+        if action * self.ordering < 0:
+            self.ordering = 0
+        if self.ordering != 0:
+            action = self.ordering
+
+        self.market_returns[self.cur_step] = ori_obs['returns']
 
         action = int(action * self.max_stake)
         position = self.positions[self.cur_step - 1]
         cost = 0
+        trading_price = ori_obs['open']
         if action < -self.min_action:
-            num_stakes = min(self.positions[self.cur_step - 1],-action)
-            position -= num_stakes
-            amount = ori_obs['open'] * num_stakes
-            cost = amount * self.trading_cost_bps
-            self.capital = self.capital + amount - cost
+            if ori_obs['low'] > self.last_obs['low']:
+                num_stakes = min(self.positions[self.cur_step - 1],-action)
+                if num_stakes > 0:
+                    trading_price = min(self.last_obs['low'],ori_obs['open'])
+                    position -= num_stakes
+                    amount = trading_price * num_stakes
+                    cost = amount * self.trading_cost_bps
+                    self.capital = self.capital + amount - cost
+                    self.ordering = 0
+                    self.buy_sell_points.append((ori_obs.name,'sell'))
+            else:
+                self.ordering = -1
 
         if action > self.min_action:
-            num_stakes = min(self.capital // ori_obs['open'],action)
-            amount = ori_obs['open'] * num_stakes
-            cost = amount * self.trading_cost_bps
-            if self.capital >= amount + cost:
-                position += num_stakes
-                self.capital = self.capital - amount - cost
+            if ori_obs['high'] > self.last_obs['high']:
+                trading_price = max(self.last_obs['high'],ori_obs['open'])
+                num_stakes = min(self.capital // trading_price // 100 * 100,action)
+                if num_stakes > 0:
+                    amount = trading_price * num_stakes
+                    cost = amount * self.trading_cost_bps
+                    if self.capital >= amount + cost:
+                        position += num_stakes
+                        self.capital = self.capital - amount - cost
+                        self.ordering = 0
+                        self.buy_sell_points.append((ori_obs.name,'buy'))
+            else:
+                self.ordering = 1
+
+        self.actions[self.cur_step] = action
 
         self.costs[self.cur_step] = cost
         self.positions[self.cur_step] = position
@@ -75,6 +110,7 @@ class MarketEnv(gym.Env):
             'ori_obs' : ori_obs
         }
         self.cur_step += 1
+        self.last_obs = ori_obs
         return (obs.values,position),reward,done,info
 
 
@@ -82,7 +118,7 @@ class MarketEnv(gym.Env):
     def result(self):
         return {
             "market_return" : log2percent(np.exp(sum(self.market_returns))),
-            "strategy_return" : log2percent(np.exp(sum(self.strategy_returns))),
+            "strategy_return" : round((self.tot_values[self.cur_step - 1] - self.tot_values[0]) / self.tot_values[0] * 100,2),
             "max_drawdown": log2percent(np.exp(np.min(np.cumsum(self.strategy_returns)))),
             "max_profit": log2percent(np.exp(np.max(np.cumsum(self.strategy_returns)))),
             "actions":self.actions,
@@ -93,18 +129,21 @@ class MarketEnv(gym.Env):
         }
 
     def plot(self):
-        fig, (ax1, ax2, ax3,ax4) = plt.subplots(4,1, figsize=(10, 6))
+        fig, (ax1, ax2, ax3,ax4,ax5,ax6) = plt.subplots(6,1, figsize=(15, 20))
         ax1.plot(self.positions[:self.cur_step], label='Position')
         ax1.set_ylabel("Position/stake")
 
         ax2.plot(self.tot_values[:self.cur_step], label='Value')
         ax2.set_ylabel("Value/CNY")
 
-        ax3.plot(np.diff(self.tot_values[:self.cur_step]) / self.tot_values[:self.cur_step - 1] * 100, label='Value')
-        ax3.set_ylabel("Returns/%")
-        print(self.capitals[:self.cur_step])
+        ax3.plot((self.tot_values[:self.cur_step] - self.tot_values[0]) / self.tot_values[0] * 100, label='Strategy Return')
+        ax3.set_ylabel("Strategy Returns/%")
         ax4.plot(self.capitals[:self.cur_step], label='Capital')
         ax4.set_ylabel("Capital/CNY")
+
+        ax5.plot(log2percent(np.exp(np.cumsum(self.market_returns))), label='Market Return')
+        ax5.set_ylabel("Market Return/%")
+
 
         ax1.legend(loc='best')
         # ax1.set_title("Market vs Strategy Returns")``
@@ -113,7 +152,12 @@ class MarketEnv(gym.Env):
         ax2.grid(True)
         ax3.grid(True)
         ax4.grid(True)
-        plt.savefig('trade_result.png', dpi=300, bbox_inches='tight')
+        ax5.grid(True)
+
+        self.data_source.plot(self.buy_sell_points)
+        plt.show()
+        # plt.savefig('trade_result.png', dpi=300, bbox_inches='tight')
+
 
     def reset(self, *, seed: int | None = None, options: dict | None = None) -> Tuple[Any, dict]:
         self.cur_step = 1
@@ -125,11 +169,14 @@ class MarketEnv(gym.Env):
         self.market_returns.fill(0)
         self.data_source.reset()
         obs,done,ori_obs = self.data_source.step()
-        return (obs.values,0)
+        info = {
+            "ori_obs":ori_obs
+        }
+        return (obs.values,0),info
 
 
 if __name__ == '__main__':
-    env = MarketEnv(252)
+    env = MarketEnv(220,start_date='20230401',end_date='20240401')
     env.reset()
     print(env.step(1))
     print(env.step(0.05))
